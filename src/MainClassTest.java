c436c17072f56210f30c484b23fb431d3a7dae03
l4h7 <lucashufeland@outlook.com>
2020-10-16 17:54:01 +0200

    test für isbn10Format

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,20 @@ class MainClassTest {
         Assumptions.assumeTrue(MainClass.pruefeIsbn10(new int[]{3,6,5,8,2,1,8,0,6,1}));
         Assumptions.assumeFalse(MainClass.pruefeIsbn10(new int[]{3,9,0,0,0,0,0,0,0,9}));
     }
+
+
+    @Test
+    void isbn10Format() {
+        Assumptions.assumeTrue(MainClass.isbn10Format("0123456789"));
+        Assumptions.assumeTrue(MainClass.isbn10Format("012345678x"));
+        Assumptions.assumeTrue(MainClass.isbn10Format("012345678X"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("012345678c"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("012345678C"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("0d23456789"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("012345678"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("1234567X"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("1234567x"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("01234567890"));
+        Assumptions.assumeFalse(MainClass.isbn10Format("0123456789X"));
+    }
 }
